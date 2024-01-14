bcab4fd39964c69e1a11e790f21a22988f89c8c1
marsh1100 <margie.b.agu17@gmail.com>
2024-01-14 16:35:10 -0500

    feat: :sparkles: Estructura AAA

--- StringManipulationTest/StringOperationTest.cs
+++ StringManipulationTest/StringOperationTest.cs
@@ -9,14 +9,14 @@ namespace StringManipulationTest
 {
     public class StringOperationTest
     {
-        [Fact]
+        [Fact(Skip = "Esta prueba no es valida en el momento. TICKET 008")]
         public void ConcatenateStrings()
         {
             var strOperations = new StringOperations();
-            var result = strOperations.ConcatenateStrings("holi","amorcito");
+            var result = strOperations.ConcatenateStrings("holi","mundo");
             Assert.NotNull(result);
             Assert.NotEmpty(result);
-            Assert.Equal("holi amorcito", result);
+            Assert.Equal("holi mundo", result);
         }
         [Fact]
         public void IsPalindrome_True()
@@ -28,7 +28,7 @@ namespace StringManipulationTest
             // Assert
             Assert.True(result);
         }
-
+        [Fact]
         public void IsPalindrome_False()
         {
             // Arrange
@@ -37,6 +37,69 @@ namespace StringManipulationTest
             var result = strOperations.IsPalindrome("cat");
             // Assert
             Assert.False(result);
+        }
+
+        [Fact]
+        public void RemoveWhitespace()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+            // Act
+            var result = strOperations.RemoveWhitespace("Josefa es bonita");
+            // Assert
+            Assert.Equal("Josefaesbonita", result);
+        }
+
+        [Theory]
+        [InlineData("gato",5,"cinco gatos")]
+        [InlineData("perro",3,"tres perros")]
+        public void QuantintyInWords(string word, int quantity, string expected )
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+            // Act
+            var result = strOperations.QuantintyInWords(word,quantity);
+            // Assert
+            //Assert.StartsWith("ocho", result);
+            Assert.Contains(word, expected);
+        }
+
+        [Fact]
+        public void  GetStringLength_Exception()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+            // Act --> No hay porque se va a saltar una excepción
+    
+            // Assert
+            Assert.ThrowsAny<ArgumentNullException>(()=> strOperations.GetStringLength(null));
+        }
+
+        [Fact]
+        public void  TruncateString_Exception()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+            // Act --> No hay porque se va a saltar una excepción
+    
+            // Assert
+            Assert.Throws<ArgumentOutOfRangeException>(()=> strOperations.TruncateString("notebook",0));
+        }
+
+        [Theory]
+        [InlineData("V",5)]
+        [InlineData("DXXV",525)]
+
+        public void FromRomanToNumber(string romanNumber, int expected)
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+        
+            // Act
+            var result = strOperations.FromRomanToNumber(romanNumber);
+        
+            // Assert
+            Assert.Equal(expected, result);
         }
     }
 }
